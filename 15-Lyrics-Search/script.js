96ab881069138a3a985f9919bd00a00e67ab2668
Michael <kllmmc23@gmail.com>
2020-10-08 21:13:15 -0600

    15-Lyrics-Search

---
+++ 15-Lyrics-Search/script.js
@@ -0,0 +1,63 @@
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const result = document.getElementById("result");
+const more = document.getElementById("more");
+
+const APIURL = "https://api.lyrics.ovh";
+
+//search by song or artist
+async function searchSongs(term) {
+  // fetch(`${APIURL}/suggest/${term}`)
+  //     .then(res => res.json())
+  //     .then(data => console.log(data))
+
+  const res = await fetch(`${APIURL}/suggest/${term}`);
+  const data = await res.json();
+  showData(data);
+}
+//show data
+function showData(data) {
+//   let output = "";
+
+//   data.data.forEach((song) => {
+//     output += `
+//             <li>
+//                 <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+//                 <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+//             </li>
+//         `;
+//   });
+
+//   result.innerHTML = `
+//         <ul class='songs'>
+//             ${output}    
+//         </ul>
+//      `;
+
+  result.innerHTML = `
+        <ul className="songs">
+            ${data.data.map(() => {
+              `
+                <li>
+                    <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+                    <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+                </li> 
+                `;
+            })}
+        </ul>
+    `;
+}
+
+//event listeners
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value.trim();
+  console.log(searchTerm);
+
+  if (!searchTerm) {
+    alert("Search something...");
+  } else {
+    searchSongs(searchTerm);
+  }
+});

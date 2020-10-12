ca038714a6efd935ca9c796fbf41be3c0fac8087
Michael <kllmmc23@gmail.com>
2020-10-12 14:08:29 -0600

    finished-drag-drop

--- 19-Sortable-List/script.js
+++ 19-Sortable-List/script.js
@@ -17,12 +17,15 @@ createList();
 
 //function to insert list into DOM
 function createList() {
-  [...richestPeople].forEach((person, index) => {
-    const listItem = document.createElement("li");
+  [...richestPeople]
+    .map((a) => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value)
+    .forEach((person, index) => {
+      const listItem = document.createElement("li");
 
-    listItem.setAttribute("data-index", index);
-
-    listItem.innerHTML = `
+      listItem.setAttribute("data-index", index);
+      listItem.innerHTML = `
         <span class="number">${index + 1}</span>
         <div class="draggable" draggable="true">
             <p class="person-name">${person}</p>
@@ -30,8 +33,70 @@ function createList() {
         </div>
       `;
 
-    listItem.push(listItem);
+      listItems.push(listItem);
 
-    draggable_list.appendChild(listItem);
+      draggable_list.appendChild(listItem);
+    });
+
+  addEventListeners();
+}
+function dragStart() {
+  dragStartIndex = +this.closest("li").getAttribute("data-index");
+  console.log(dragStartIndex);
+}
+function dragEnter() {
+  this.classList.add("over");
+}
+function dragLeave() {
+  this.classList.remove("over");
+}
+function dragOver(e) {
+  e.preventDefault();
+}
+function dragDrop() {
+  const dragEndIndex = +this.getAttribute("data-index");
+  console.log(dragEndIndex);
+
+  swapItems(dragStartIndex, dragEndIndex);
+
+  this.classList.remove("over");
+}
+
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector(".draggable");
+  const itemTwo = listItems[toIndex].querySelector(".draggable");
+
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
+
+  checkOrder();
+}
+
+function checkOrder() {
+  listItems.forEach((listItem, index) => {
+    const personName = listItem.querySelector(".draggable").innerText.trim();
+    if (personName !== richestPeople[index]) {
+      listItem.classList.add("wrong");
+    } else {
+      listItem.classList.remove("wrong");
+      listItem.classList.add("right");
+    }
   });
 }
+
+function addEventListeners() {
+  const draggables = document.querySelectorAll(".draggable");
+  const dragListItems = document.querySelectorAll(".draggable-list li");
+
+  draggables.forEach((draggable) => {
+    draggable.addEventListener("dragstart", dragStart);
+  });
+
+  dragListItems.forEach((draggable) => {
+    draggable.addEventListener("dragover", dragOver);
+    draggable.addEventListener("drop", dragDrop);
+    draggable.addEventListener("dragenter", dragEnter);
+    draggable.addEventListener("dragleave", dragLeave);
+  });
+}
+check.addEventListener("click", checkOrder);

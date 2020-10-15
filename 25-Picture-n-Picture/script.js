043900c449fa07748f71e4f9da180e3c8cda4440
Michael <kllmmc23@gmail.com>
2020-10-14 19:19:04 -0600

    25-finished

--- 25-Picture-n-Picture/script.js
+++ 25-Picture-n-Picture/script.js
@@ -3,11 +3,25 @@ const button = document.getElementById("button");
 
 async function selectMediaStream() {
   try {
-    const mediaStream = await navigator.mediaDevices.getDisplayMedia()
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+      };
+      
   } catch (error) {
-    console.log("Error+++>>>", error);
+    
   }
 }
+
+//event listener
+button.addEventListener("click", async () => {
+  button.disabled = true;
+
+  await videoElement.requestPictureInPicture();
+
+  button.disabled = false;
+});
 
 //on load
 selectMediaStream();

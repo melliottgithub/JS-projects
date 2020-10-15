fb70d326e6f828af90b6e778e7f70e19df536878
Michael <kllmmc23@gmail.com>
2020-10-14 18:14:03 -0600

    25-Picture-n-Picture

--- 25-Picture-n-Picture/script.js
+++ 25-Picture-n-Picture/script.js
@@ -1,4 +1,13 @@
 const videoElement = document.getElementById("video");
 const button = document.getElementById("button");
 
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia()
+  } catch (error) {
+    console.log("Error+++>>>", error);
+  }
+}
 
+//on load
+selectMediaStream();

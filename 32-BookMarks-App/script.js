831bf27aa40b057f1dcd57b38e2eec1d1292a125
Michael <kllmmc23@gmail.com>
2020-10-21 12:40:41 -0600

    33-Video-Player

--- 32-BookMarks-App/script.js
+++ 32-BookMarks-App/script.js
@@ -32,6 +32,9 @@ function validateExp(nameValue, urlValue) {
 }
 //build bookmarks onto DOM
 function buildBmarks() {
+  //remove before updating
+  bookmarksContainer.textContent = "";
+  //build items
   bookmarks.forEach((bookmark) => {
     //destructuring bookmarks
     const { name, url } = bookmark;
@@ -41,6 +44,28 @@ function buildBmarks() {
     //delete icon x
     const closeIcon = document.createElement("i");
     closeIcon.classList.add("fas", "fa-times");
+    closeIcon.setAttribute("id", "delete-bookmark");
+    closeIcon.setAttribute("title", "Delete Bookmark");
+    closeIcon.setAttribute("onclick", `deleteBmark("${url}")`);
+    //create <div class="name">
+    const linkInfo = document.createElement("div");
+    linkInfo.classList.add("name");
+    //create image
+    const img = document.createElement("img");
+    img.setAttribute(
+      "src",
+      "https://images.unsplash.com/photo-1527176930608-09cb256ab504?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    );
+    img.setAttribute("alt", "bookmark");
+    // Link <a></a>
+    const a = document.createElement("a");
+    a.setAttribute("href", `${url}`);
+    a.setAttribute("target", "_blank");
+    a.textContent = name;
+    // append all
+    linkInfo.append(img, a);
+    item.append(closeIcon, linkInfo);
+    bookmarksContainer.appendChild(item);
   });
 }
 //fetch data local storage
@@ -60,6 +85,17 @@ function fetchBmarks() {
   }
   buildBmarks();
 }
+//delete bookmark
+function deleteBmark(url) {
+  console.log(url);
+  bookmarks.forEach((bookmark, i) => {
+    if (bookmark.url === url) {
+      bookmarks.splice(i, 1);
+    }
+  });
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  fetchBmarks();
+}
 //handle Data
 function storeBookmark(e) {
   e.preventDefault();
@@ -75,10 +111,10 @@ function storeBookmark(e) {
   };
   bookmarks.push(bookmark);
   localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  fetchBmarks()
   bookmarkForm.reset();
   websiteNameEl.focus();
 }
-
 /* Event Listeners */
 modalShow.addEventListener("click", showModal);
 modalClose.addEventListener("click", closeModal);
@@ -86,6 +122,5 @@ window.addEventListener("click", (e) =>
   e.target === modal ? closeModal() : false
 );
 bookmarkForm.addEventListener("submit", storeBookmark);
-
 //on load
 fetchBmarks();
